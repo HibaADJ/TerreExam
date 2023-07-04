0f9f54995c64c06524e681a977e81b9ca7546ca7
HibaADJ <adjmihibat@gmail.com>
2023-07-04 14:50:06 -0400

    file name changed

---
+++ terre01.js
@@ -0,0 +1,13 @@
+  /* */
+
+  let filePath = `Filename is ${__filename}`
+  let fileName = " ";
+  for (i=filePath.length; i>0; i--){
+  if (filePath.charAt(i)=="/"){
+  break;
+  }
+  else{
+  fileName = filePath.charAt(i).concat(fileName);
+  }
+  }
+  console.log(fileName);
---
+++ terre02.js
@@ -0,0 +1,4 @@
+  /*Program that displays the arguments it receives line by line.
+  input : Arguments 
+  output :  Display Arguments*/
+  console.log(process.argv);
